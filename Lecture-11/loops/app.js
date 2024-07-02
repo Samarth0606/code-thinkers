9f8aa97ace8f2d311c963654184c3c099feabc27
Samarth <officialdevelopersamarth@gmail.com>
2024-07-02 13:50:13 +0530

    lecture 11 codes

---
+++ Lecture-11/loops/app.js
@@ -0,0 +1,78 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+
+// for(let i = 0; i <= 6; i++){
+//     console.log(i)
+// }
+
+// let i = 0;
+
+// while(i<=6){
+//     console.log(i);
+//     i++;
+// }
+
+// 5 -> for , while , do-while , 
+//for-of => array
+//for-in => object
+
+// array => ds => heterogenous => collection of data
+// let a = [];
+// let arr = [10,20,30,40,50]; //homo array
+// let arr2 = [10,"sam",true,70,false]; //hetero array
+
+// object => collection of properties => properties = key : value
+// let person = {
+//     // collection of properties = key : value
+//     name : "samarth" ,
+//     age : 16 ,
+//     isMale : true
+// };
+
+//arr -> heter(6)
+// obj -> car(4)
+
+// ----------------------
+
+
+// let arr = [10,20,"sam","vohra" , null , undefined , true , false]
+
+// console.log(arr)
+// console.log(arr[3])
+// console.log(arr[6])
+
+// -----
+
+// let obj = {
+//     a:10,
+//     b:20,
+//     c:30,
+//     d:40
+// }
+
+//dot notation
+// console.log(obj.a , obj.b)
+// console.log(obj.c)
+// console.log(obj['1'])
+
+//keys ko string dena hai ? bts => jitna bhi aapka object hai unki keys string ki tarah store hoti hai
+// console.log(obj)
+// //array syntax
+// console.log(obj['a']) //10
+// console.log(obj['b']) //20
+// console.log(obj[a]) //error
+// console.log(obj[b]) //error
+
+// ---------
+
+// let arr = [10,20,true,false,[100,[10002,10005,10003],103],40 , {a:10,b:20} , null]
+// console.log(arr[4])
+// console.log(arr[4][1])
+// console.log(arr[4][1][2])
+// console.log(arr[6])
+// console.log(arr[6]['a'])
+// console.log(arr[6]['b'])
---
+++ Lecture-11/math-methods/app.js
@@ -0,0 +1,129 @@
+
+
+// -------
+
+// if(true){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+// -----
+ 
+// if(true){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// ------
+
+// let score = 34;
+// let final = score == 100;
+// console.log(final)
+
+
+// == -> value check kro
+// === -> value check krt + datatype(primitive)
+
+
+// let a = 1
+
+// let b = '1'
+// console.log(a==b) //general checking/equlaity
+// console.log(a===b) //strict checking/equlaity
+
+
+
+// let a = 10;
+// let final = a === '10';
+// if(final){
+//     console.log("i am don")
+// }else{
+//     console.log("you are don")
+// }
+
+
+//not -> !
+// let a = 10;
+// let final = a !== '10'; 
+// if(final){
+//     console.log("i am don")
+// }else{
+//     console.log("you are don")
+// }
+
+// console.log(100>=99)
+// console.log(100>=100)
+// console.log(100==100)
+// console.log(100<=100)
+// console.log(100<=101)
+// console.log(100<101)
+// console.log(102>101)
+
+// --------------------------
+
+// function samarth(){
+//     console.log("hello sam")
+// }
+
+// samarth()
+// samarth()
+// samarth()
+
+// ------
+
+// function samy(a,b){
+//     console.log(a)
+//     console.log(b)
+//     console.log(a+b)
+// }
+// samy(10,20)
+// samy(30,50)
+
+
+// -------------------------
+// let person = {
+//     naam:"swaggy boi",
+//     umar: 24,
+//     lang : "punjabi",
+//     favDialogue : function(){}
+// }
+
+// console.log(Math)
+
+// -------
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// function counter(arr){
+//     for(let item=0;item<10;item++){
+//        console.log(arr[item])
+//     }
+// }
+// counter(arr)
+
+//for-of => array
+//for-in => objects
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// function counter(arr){
+//    for(let item of arr){
+//     console.log(item)
+//    }
+// }
+// counter(arr)
+// -------
+
+// let obj = {
+//     a:10,
+//     b:20,
+//     c:30,
+//     d:40
+// }
+
+// for(let item in obj){
+//     console.log(item)
+//     console.log(obj[item])
+// }
