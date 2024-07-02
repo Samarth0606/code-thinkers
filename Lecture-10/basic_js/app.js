342962c48a00cdaa1d103f73efa62d1b44aa445d
Samarth <officialdevelopersamarth@gmail.com>
2024-07-02 12:18:15 +0530

    lrcture 10 codes

---
+++ Lecture-10/basic_js/app.js
@@ -0,0 +1,53 @@
+// console.log("yaha se krenge kaam");
+
+// string
+
+// let firstName = "rahul";
+// let lastName = "PRAJAPATI";
+let age = 25;
+
+// console.log(firstName);
+// console.log(lastName);
+
+// concatenate
+// console.log(firstName + lastName);
+
+// let spac = " ";
+// console.log(firstName + spac + lastName);
+// console.log(firstName + " " + lastName);
+
+// backeticks
+
+// console.log(`my fisrtname is ${firstName} and lastname is ${lastName} and my age s${age} `)
+
+
+// length
+// console.log(firstName.length);
+
+// uppercase()
+//LowerCase()
+
+// console.log(firstName.toUpperCase());
+// console.log(lastName.toLowerCase());
+
+// let lastName = "     PRAJAPATI       ";
+// let ans = lastName.trim();
+// console.log(lastName.length);
+// console.log(ans);
+// console.log(ans.length);
+// console.log(first)
+
+// number
+let a  = 10;
+let b = 45.4;
+let c = 45.456703;
+let d = 2+5;
+let e = 2*10;
+let f = 2%4;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
